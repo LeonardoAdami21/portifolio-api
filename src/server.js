a43d31aeae78b100e882b3093195c951ae150bb2
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-08-31 15:29:54 -0300

    feat/configurations, configurações iniciais e dependencias instaladas para funcionar a api.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDOC from "../swagger.json" assert { type: "json" };
 
 const app = express();
 
