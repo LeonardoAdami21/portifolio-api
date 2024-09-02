57f3ddda91b6af8d86800a238c6114592eacf9a5
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-02 09:17:57 -0300

    dev, implement css to se in vercel build.

--- src/server.js
+++ src/server.js
@@ -5,11 +5,18 @@ import swaggerDOC from "../swagger.json" assert { type: "json" };
 import { appPort } from "./env/envoriment.js";
 import mailerRouter from "./mail/mail.router.js";
 
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDOC));
+app.use(
+  "/api",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDOC, { cssUrl: CSS_URL }),
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/", mailerRouter);
